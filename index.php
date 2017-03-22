6af8409333c3102a71e9d3128c8b3eb9c5f8896d
Zkstorczyk <zkstorczyk@gmail.com>
2017-03-22 10:03:35 +0100

    Podstawowe Zmiany
    Dodano pierwszy Rzeczy

---
+++ index.php
@@ -0,0 +1,33 @@
+﻿<?php
+
+$d = dir(getcwd());
+
+echo "Handle: " . $d->handle . "<br>";
+echo "Path: " . $d->path . "<br>";
+
+while (($file = $d->read()) !== false){ 
+  echo "filename: " . $file . "<br>"; 
+} 
+$d->close(); 
+
+echo "dsdasd<br>";
+
+
+if ($handle = opendir('/home/u323117235/public_html/test/Eksperymenty')) {
+    echo "Directory handle: $handle\n";
+    echo "Entries:<br>";
+
+    /* This is the correct way to loop over the directory. */
+    while (false !== ($entry = readdir($handle))) {
+        echo "$entry<br>";
+		echo "<a href='Eksperymenty/".$entry."'>$entry</a>";
+    }
+
+    /* This is the WRONG way to loop over the directory. */
+    while ($entry = readdir($handle)) {
+        echo "$entry<p>asd</p>";
+    }
+
+    closedir($handle);
+}
+?>
